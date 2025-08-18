580e48b02f6f2bff125e4e752ac47bb51c4d9918
joshuaelan12 <joshuaelangwe12@gmail.com>
2025-08-18 11:54:55 -0400

    Ui Update 4

--- src/Pages/app-pages/internet/Internet.js
+++ src/Pages/app-pages/internet/Internet.js
@@ -1,8 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Sidebar from '../side-bar/Sidebar';
+import { FaToggleOff } from 'react-icons/fa';
+
+import './Internet.css';
 
 const Internet = () => {
-  return (
-    <div>Internet</div>
+    const [toggleSideBar, setToggleSideBar] = useState(false);
+
+    const handleToggleSideBar = () => {
+  
+      setToggleSideBar(!toggleSideBar);
+  
+    }
+
+    const [selectedValue, setSelectedValue] = useState("");
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      alert(`You selected: ${selectedValue}`);
+    };
+  
+  
+    return (
+      <div className='internet'>
+          <span className={toggleSideBar ? 'toggle-on' : 'toggle-off'}>
+          <Sidebar isOpen={toggleSideBar} toggleSideBar={handleToggleSideBar}/>
+          </span>
+          <div className={toggleSideBar ? 'internet-content toggle-off' : 'internet-content toggle-on'}>
+              <div className='sidebar-toggle-button' onClick={handleToggleSideBar}>
+              <FaToggleOff size={25} />
+              </div>
+           
+
+            <div className='internet-card'>
+              
+                <h2>Buy Internet Data</h2>
+              
+              <form className='internet-form' onSubmit={handleSubmit}>
+              
+              <label>Provider</label>
+                <select className='select-package' name='provider' id='provider' 
+                onChange={(e) => {
+                  setSelectedValue(e.target.value);
+                }
+            }                               
+                required>
+                    <option value=''>Select Provider</option>
+                    <option value='MTN'>MTN</option>
+                    <option value='ORANGE'>ORANGE </option>
+                    <option value='CAMTEL'>CAMTEL</option>
+                    <option value='NEXTEL'>NEXTEL </option>
+                    
+                    </select>
+
+                 <label>Bundle Package</label>
+                <select className='select-package' name='package' id='package' 
+                onChange={(e) => {
+                  setSelectedValue(e.target.value);
+                }
+            }                               
+                required>
+                    <option value=''>Select Package</option>
+                    
+                    
+                    </select>
+
+                <label>Mobile Phone Number</label>
+                <input name='phone' id='phone' type='text' 
+                placeholder='e.g., 67000000'></input>
+
+                <p className='subtitle'>Enter the 9-digit number to be charged (without country code)</p>
+
+                
+                <button className='pay-btn'>Pay Now</button>
+
+                
+              </form>
+
+            </div>
+         </div>
+
+    </div>
   )
 }
 
